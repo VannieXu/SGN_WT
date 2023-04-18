13843e31a14dcb7363e808abc78b1d768b5c9303
VannieXu <50305431+VannieXu@users.noreply.github.com>
2023-04-18 14:52:51 -0400

    Update pc2.py

--- pc2.py
+++ pc2.py
@@ -21,3 +21,25 @@ sns.regplot(
     data=df_p, x="pc2", y=g_,
     scatter=False, truncate=False, order=3,
 )
+
+df['time'] = round(adata.obs['PC2'],1).to_list()
+df_p = df.groupby('time',as_index = False).mean()
+
+plt.plot()
+sns.scatterplot(data=df_p, x="time", y='Ttn',label = 'Ttn',s = 5)
+sns.regplot(
+    data=df_p, x="time", y='Ttn',
+    scatter=False, truncate=False, order=3,
+)
+sns.scatterplot(data=df_p, x="time", y="Lypd1",label = 'Lypd1',s = 5)
+sns.regplot(
+    data=df_p, x="time", y='Lypd1',
+    scatter=False, truncate=False, order=3,
+)
+sns.scatterplot(data=df_p, x="time", y="Calb2",label = 'Calb2',s = 5)
+sns.regplot(
+    data=df_p, x="time", y='Calb2',
+    scatter=False, truncate=False, order=3,
+)
+plt.xlabel('PC2')
+plt.ylabel('log10(expression)')
